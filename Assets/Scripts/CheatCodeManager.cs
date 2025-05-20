922e667d98e0faf74f440382eb094f84e7bab77a
AlphaActual <tin.priti@gmail.com>
2025-05-20 09:35:16 +0200

    added cheat code manager

---
+++ Assets/Scripts/CheatCodeManager.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using Invector.vCharacterController; // Make sure you have this using statement
+
+public class CheatCodeManager : MonoBehaviour
+{
+    // Add more cheats as needed
+    private string[] cheatCodes = { "superspeed", "normalspeed" };
+    private int maxCheatLength = 15; // Longest cheat code length ("normalspeed" is 15)
+    private string inputBuffer = "";
+
+    // Reference to the player controller
+    private vThirdPersonController playerController;
+    private float normalSprintSpeed = 6f;
+    private float cheatSprintSpeed = 60f;
+    private bool superSpeedActive = false;
+
+    void Start()
+    {
+        // Find the player controller in the scene
+        var playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj != null)
+        {
+            playerController = playerObj.GetComponent<vThirdPersonController>();
+            // Assign normal sprint speed
+            normalSprintSpeed = playerController.freeSpeed.sprintSpeed;
+        }
+    }
+
+    void Update()
+    {
+        foreach (char c in Input.inputString)
+        {
+            if (char.IsLetter(c))
+            {
+                inputBuffer += char.ToLower(c);
+                if (inputBuffer.Length > maxCheatLength)
+                    inputBuffer = inputBuffer.Substring(inputBuffer.Length - maxCheatLength);
+                CheckCheatCodes();
+            }
+        }
+    }
+
+    void CheckCheatCodes()
+    {
+        if (inputBuffer.EndsWith("superspeed") && !superSpeedActive)
+        {
+            ActivateSuperSpeed();
+        }
+        else if (inputBuffer.EndsWith("normalspeed") && superSpeedActive)
+        {
+            ActivateNormalSpeed();
+        }
+        // Add more cheats here
+    }
+
+    void ActivateSuperSpeed()
+    {
+        if (playerController != null)
+        {
+            // Set sprintSpeed to cheat value
+            var speed = playerController.freeSpeed;
+            speed.sprintSpeed = cheatSprintSpeed;
+            playerController.freeSpeed = speed;
+            superSpeedActive = true;
+            Debug.Log("Super Speed Cheat Activated!");
+        }
+    }
+
+    void ActivateNormalSpeed()
+    {
+        if (playerController != null)
+        {
+            // Set sprintSpeed to normal value
+            var speed = playerController.freeSpeed;
+            speed.sprintSpeed = normalSprintSpeed;
+            playerController.freeSpeed = speed;
+            superSpeedActive = false;
+            Debug.Log("Normal Speed Cheat Activated!");
+        }
+    }
+}
